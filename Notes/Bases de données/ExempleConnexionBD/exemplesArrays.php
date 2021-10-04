502b2bf3ca8bac0150abfdebbf05cec6351b1fe9
choquitofrito <zyriab@gmail.com>
2021-10-04 10:29:12 +0200

    ok

--- Notes/Bases de données/ExempleConnexionBD/exemplesArrays.php
+++ Notes/Bases de données/ExempleConnexionBD/exemplesArrays.php
@@ -13,90 +13,96 @@
     // exemples d'accés aux arrays
 
     // array simple, clés numériques
-    $noms = ['Julie', 'Judy', 'Anaïs'];
-    echo "<ul>";
-    foreach ($noms as $val) {
-        echo "<li>" . $val . "</li>";
-    }
-    echo "</ul>";
+    // $noms = ['Julie', 'Judy', 'Anaïs'];
+    // echo "<ul>";
+    // foreach ($noms as $val) {
+    //     echo "<li>" . $val . "</li>";
+    // }
+    // echo "</ul>";
 
-    $film1 = [
-        'titre' => 'Rocky 1',
-        'nationalite' => 'USA',
-        'duree' => 120
-    ];
+    // $film1 = [
+    //     'titre' => 'Rocky 1',
+    //     'nationalite' => 'USA',
+    //     'duree' => 120
+    // ];
 
-    var_dump($film1);
+    // var_dump($film1);
 
-    // afficher USA
-    echo $film1['nationalite'];
+    // // afficher USA
+    // echo $film1['nationalite'];
 
-    // erreur: pas d'index numériques!
-    // echo $film1[0];
+    // // erreur: pas d'index numériques!
+    // // echo $film1[0];
 
-    $vals = [10, 20, 30];
+    // $vals = [10, 20, 30];
 
-    // si on veut obtener les clés d'un array (n'importe le type)
-    $clesFilm1 = array_keys($film1);
-    var_dump($clesFilm1);
+    // // si on veut obtener les clés d'un array (n'importe le type)
+    // $clesFilm1 = array_keys($film1);
+    // var_dump($clesFilm1);
 
-    $clesVals = array_keys($vals);
-    var_dump($clesVals);
+    // $clesVals = array_keys($vals);
+    // var_dump($clesVals);
 
-    // accés au résultat d'une bd
+    // // accés au résultat d'une bd
     include "./config/db.php";
 
     $bdd = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT .
         ';dbname=' . DBNAME . ';charset='
         . DBCHARSET, DBUSER, DBPASS);
 
-    $sql = "SELECT * FROM actor";
+    // $sql = "SELECT * FROM actor";
+    // $requete = $bdd->prepare($sql);
+    // $requete->execute();
+    // $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    // // afficher avec var_dump
+    // var_dump ($resultat);
+    // // exemples :
+    // // 1. afficher le nom du deuxième acteur
+    // echo "<br>";
+    // echo $resultat[1]['first_name'];
+
+    // // 2. mettre le 3ème acteur dans un array
+    // $arrayActeur = $resultat[2];
+    // // afficher son nom de famille
+    // echo $arrayActeur['last_update'];
+
+    // // Obtenir uniquement les en-têtes
+    // $entetes = array_keys($resultat[0]);
+    // var_dump ($entetes);
+
+    // // Parcourir le résultat et afficher le contenu sous la forme
+    // // clé : valeur
+    
+    // // first_name : Toto
+    // // last_name : Dupont
+
+    // // ex avec for :
+    // for ($i = 0; $i < count ($resultat); $i++){
+    //     $unActeur = $resultat[$i];
+    //     echo "<br>first_name : " . $unActeur['first_name'];  
+    //     echo "<br>last_name : " .  $unActeur['last_name'];
+    // }
+    // // ex avec foreach (même chose)
+    // foreach ($resultat as $val){
+    //     echo "<br>first_name : " . $val['first_name'];  
+    //     echo "<br>last_name : " .  $val['last_name'];
+    // }
+
+    // // ex avec foreach
+    // foreach ($resultat as $arrayActeur){
+    //     foreach ($arrayActeur as $cleArrayActeur => $valeurArrayActeur){
+    //         echo "<br>". $cleArrayActeur . " : ". $valeurArrayActeur;
+    //     }
+    // }
+
+
+    $sql = "SELECT COUNT(*) AS totalActeurs FROM actor";
     $requete = $bdd->prepare($sql);
     $requete->execute();
     $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
-
-    // afficher avec var_dump
+    echo "<br>Le nombre total d'acteurs est : " . $resultat[0]['totalActeurs'];
     var_dump ($resultat);
-    // exemples :
-    // 1. afficher le nom du deuxième acteur
-    echo "<br>";
-    echo $resultat[1]['first_name'];
-
-    // 2. mettre le 3ème acteur dans un array
-    $arrayActeur = $resultat[2];
-    // afficher son nom de famille
-    echo $arrayActeur['last_update'];
-
-    // Obtenir uniquement les en-têtes
-    $entetes = array_keys($resultat[0]);
-    var_dump ($entetes);
-
-    // Parcourir le résultat et afficher le contenu sous la forme
-    // clé : valeur
-    
-    // first_name : Toto
-    // last_name : Dupont
-
-    // ex avec for :
-    for ($i = 0; $i < count ($resultat); $i++){
-        $unActeur = $resultat[$i];
-        echo "<br>first_name : " . $unActeur['first_name'];  
-        echo "<br>last_name : " .  $unActeur['last_name'];
-    }
-    // ex avec foreach (même chose)
-    foreach ($resultat as $val){
-        echo "<br>first_name : " . $val['first_name'];  
-        echo "<br>last_name : " .  $val['last_name'];
-    }
-
-    // ex avec foreach
-    foreach ($resultat as $arrayActeur){
-        foreach ($arrayActeur as $cleArrayActeur => $valeurArrayActeur){
-            echo "<br>". $cleArrayActeur . " : ". $valeurArrayActeur;
-        }
-    }
-
-
 
     ?>
 
