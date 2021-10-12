dd0642451bf65c4a236c421386616316e224fae4
choquitofrito <zyriab@gmail.com>
2021-10-12 10:12:06 +0200

    ok

--- Notes/Bases de données/ExempleConnexionBD/insertionTraitement.php
+++ Notes/Bases de données/ExempleConnexionBD/insertionTraitement.php
@@ -23,6 +23,37 @@
         echo $e->getMessage();
         die();
     }
+    // juste pour vérifier si la connexion est ok et le formulaire est bien construit
+    // var_dump ($bdd);
+    // var_dump ($_POST);
+
+    // stocker le contenu du form dans de variables (chaque input dans une variable)
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $last_update = $_POST['last_update'];
+
+    // filtrer eventuellement avec filter_vars et filter_input
+    // pour augmenter la sécurité 
+
+
+    // créer la requête, dans ce cas un insert
+    $sql = "INSERT INTO actor (first_name, last_name, last_update) ".
+        "VALUES (:first_name, :last_name, :last_update)";
+    
+    $requete = $bdd->prepare($sql); // renvoie un PDOStatement
+    $requete->bindValue(":first_name", $first_name);
+    $requete->bindValue(":last_name", $last_name);
+    $requete->bindValue(":last_update", $last_update);
+    var_dump ($requete->errorInfo());
+    var_dump ($bdd->errorInfo());
+    $requete->execute();
+   
+
+
+
+
+
+
 
 
     ?>
