c8517c800d027f131b01a1fea0183ebea3cdd9d8
choquitofrito <zyriab@gmail.com>
2021-10-13 15:06:15 +0200

    ok

---
+++ Notes/Bases de données/ExempleConnexionBD-OOP/formInsertPays.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./traitementInsertPays.php" method="POST">
+        Nom du Pays<input type="text" name="country">
+        Last Update<input type="date" name="last_update">
+        <input type="submit" value="Enregistrer">
+    </form>
+</body>
+</html>
